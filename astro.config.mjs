6e3682351d8ed507703e9945e13e691dfcbc197b
shahidify <shahid.manit@gmail.com>
2025-06-25 14:18:43 -0500

    github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   output: 'static',
   trailingSlash: 'always',
-  site: 'https://shahidify.tech/',
+  site: 'https://shahidify.github.io/',
 
   // Single page, no prefetch needed
   prefetch: false,
